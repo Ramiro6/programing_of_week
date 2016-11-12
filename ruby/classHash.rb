35d8a750d56a1e6b0ea3f6c282cda743c84a132f
Ramiro <ramiroruiz578@gmail.com>
2016-11-12 18:32:07 +0100

    second

---
+++ ruby/classHash.rb
@@ -0,0 +1,15 @@
+  #hash
+
+  car = {
+    "sound" => "RUUUNNN!!", engine: "URURURURURURUR!!!"
+  }
+
+  puts car["sound"]
+  puts car[:engine]
+
+  battery = {
+    "sound" => "hit-hat", Sound.new({text:"bom"})
+  }
+
+  puts battery["sound"]
+  puts battery[:text]
