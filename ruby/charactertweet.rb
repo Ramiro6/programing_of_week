215ed0d8c6f6233667fbaf6c57f6f3c89059fc05
Ramiro <ramiroruiz578@gmail.com>
2016-11-22 15:34:08 +0100

    first

---
+++ ruby/charactertweet.rb
@@ -0,0 +1,9 @@
+  #idea character tweet. your cannot send one tweet from more out of 6 character
+
+  var = gets.chomp
+
+  if var.length >= 6
+    puts "you tweet send"
+  else
+    puts "your tweet is sendend"
+  end
